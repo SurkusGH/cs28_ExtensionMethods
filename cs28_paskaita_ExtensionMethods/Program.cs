cfa0e5abf9b40dc09a03d7d920ced7c450ca354b
SurkusGH <v.surkus@me.com>
2022-02-15 21:14:08 +0200

    Add project files.

---
+++ cs28_paskaita_ExtensionMethods/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cs28_paskaita_ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cs28_PASKAITA_ExtensionMethods!");
+            #region TEORIJA - EXTENSION METHODS
+            // Extension metodas leidžia pridėti nauj1 funkcionalum1 prie jau egzistuojan2io tipo;
+            // Extension metodai kvie2iami ant extendinamo tipo;
+            int x = 0;
+            // Kas yra instance metodai? Kaip jie kviečiami?  Bus kviečiami per objektą klasės.
+            // Duomenų tipo papildymas nauju funkcionalumu
+
+            // Extension metodai aprašomi kaip statiniai, bet kviečiami kaip instance tipo metodai.
+            // This referuoja į esamą instance klasės ir taip pat kaip modyfier pirmam paremetro metodui;
+
+            string s = "55555 4444 333 22 1";
+            Console.WriteLine(s.WordCount());
+            #endregion
+
+            #region Solution_01
+            // Parašykite extension metodą sveikiesiems skaičiams, kuris grąžins bool tipo kintamąjį nusakantį ar skaičius buvo teigiamas ar ne.
+            Console.WriteLine($"Skaičius yra teigiamas? 10: {10.EvalueteIfGivenIntegerIsMoreThan0()}");
+            Console.WriteLine($"Skaičius yra teigiamas? 0: {0.EvalueteIfGivenIntegerIsMoreThan0()}");
+            Console.WriteLine($"Skaičius yra teigiamas? -5: {(-5).EvalueteIfGivenIntegerIsMoreThan0()}");
+            Console.WriteLine();
+
+            // Parašykite extension metodą sveikiesiems skaičiams, kuris grąžins bool tipo kintamąjį nusakantį ar skaičius buvo lyginis ar ne.
+            Console.WriteLine($"Ar skaičius yra sveikojo tipo? 10: {(10).EvalueteIfGivenIntegerDividesBy2()}");
+            Console.WriteLine($"Ar skaičius yra sveikojo tipo? 5: {(5).EvalueteIfGivenIntegerDividesBy2()}");
+            Console.WriteLine();
+
+            // Parašykite extension metodą sveikiesiems skaičiams, kuris grąžins bool tipo kintamąjį nusakantį ar skaičius perduotas per parametrą yra didesnis ar ne.
+            Console.WriteLine($"Ar pirmas skaičius didesnis už antrą? 1, 2: {(1, 2).IsFirstNumberBiggerThanSecondOne()}");
+            Console.WriteLine($"Ar pirmas skaičius didesnis už antrą? 2, 1: {(2, 1).IsFirstNumberBiggerThanSecondOne()}");
+            Console.WriteLine();
+
+            // Parašykite extension metodą string tipui, kuris grąžins bool tipo kintamąjį nusakantį ar sakinyje yra tarpų ar ne.
+            Console.WriteLine($"Ar sakinyje yra tarpų? Rytas Diena Vakaras: {("Rytas Diena Vakaras").IsWhiteSpacesPresent()}");
+            Console.WriteLine($"Ar sakinyje yra tarpų? Rytas+Diena+Vakaras: {("Rytas+Diena+Vakaras").IsWhiteSpacesPresent()}");
+            Console.WriteLine();
+
+            // Parašykite extension metodą string tipui su parametrais fullname, yearOfBirth ir domain,
+            // metodas grąžins rezultatą kaip el.pašto adresą. Pvz.: “vardenispavardenis1990 @gmail.com”
+            Console.WriteLine($"Paduodu: 4 stringus: Vytautas, Surkus, 2000, whateva: {("Vytautas", "Surkus", "2000", "whateva").ConcatStringsIntoAnEmail()}");
+            Console.WriteLine($"Paduodu: 1 stringą: 'Vytautas, Surkus, 2000, whateva': {("Vytautas, Surkus, 2000, whateva").ConcatStringsIntoAnEmailTurbo()}");
+
+            // Parašykite extension metodą FindAndReturnIfEqual List<T> tipui,
+            // kuris priimtu T tipo objektą kaip parametrą ir grąžintų tokį patį, jeigu sąraše jis egzistuoja.
+            var list = new List<string>();
+            list.Add("Diena");
+            list.Add("Rytas");
+            list.Add("Vakaras");
+
+            Console.WriteLine($"Sąrašas: Diena, Rytas, Vakaras; Tikrinu Diena: {list.FindAndReturnIfEqual("Diena")}");
+            Console.WriteLine($"Sąrašas: Diena, Rytas, Vakaras; Tikrinu Naktis: {list.FindAndReturnIfEqual("Naktis")}");
+
+            // Parašykit extension metodą EveryOtherWord List<T> tipui, kuris grąžintų sąrašą sudaryta iš kas antro elemento.
+            var list2 = new List<int>();
+            list2.Add(1);
+            list2.Add(2);
+            list2.Add(3);
+            list2.Add(4);
+            list2.Add(5);
+            list2.Add(6);
+            list2 = list2.RemoveEverySecondListItem();
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+        }
+
+    }
+    #region Extension metodo pavizdys
+    public static class StringExtensions
+    {
+        public static int WordCount(this string str) // <-- čia this nutaiko į objektą iš kurio kviečiama
+        {
+            return str.Split(new char[] { ' ', '.', '?', }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+    #endregion
+}
---
+++ cs28_paskaita_ExtensionMethods/Solutions/Solution_01.cs
@@ -0,0 +1,148 @@
+﻿
+using System.Collections.Generic;
+using System.IO;
+
+namespace cs28_paskaita_ExtensionMethods
+{
+    public static class Solution_01
+    {
+        // Parašykite extension metodą sveikiesiems skaičiams, kuris grąžins bool tipo kintamąjį nusakantį ar skaičius buvo teigiamas ar ne.
+        public static bool EvalueteIfGivenIntegerIsMoreThan0(this int input)
+        {
+            if (input>=0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Parašykite extension metodą sveikiesiems skaičiams, kuris grąžins bool tipo kintamąjį nusakantį ar skaičius buvo lyginis ar ne.
+        public static bool EvalueteIfGivenIntegerDividesBy2(this int input)
+        {
+            if (input % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Parašykite extension metodą sveikiesiems skaičiams, kuris grąžins bool tipo kintamąjį nusakantį ar skaičius perduotas per parametrą yra didesnis ar ne.
+        public static bool IsFirstNumberBiggerThanSecondOne(this (int, int) input)
+        {
+            if (input.Item1 > input.Item2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Parašykite extension metodą string tipui, kuris grąžins bool tipo kintamąjį nusakantį ar sakinyje yra tarpų ar ne.
+        public static bool IsWhiteSpacesPresent(this string input)
+        {
+            char whiteSpace = ' ';
+            if (input.Contains(whiteSpace))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Parašykite extension metodą string tipui su parametrais fullname, yearOfBirth ir domain,
+        // metodas grąžins rezultatą kaip el.pašto adresą. Pvz.: “vardenispavardenis1990 @gmail.com”
+        public static string ConcatStringsIntoAnEmail(this (string, string, string, string) input)
+        {
+            return $"{input.Item1}{input.Item2}{input.Item3}@{input.Item4}.com";
+        }
+        public static string ConcatStringsIntoAnEmailTurbo(this string input)
+        {
+            string[] inputArrayList = input.Split(", ");
+            return $"{inputArrayList[0]}{inputArrayList[1]}{inputArrayList[2]}@{inputArrayList[3]}.com";
+        }
+
+        // Parašykite extension metodą FindAndReturnIfEqual List<T> tipui,
+        // kuris priimtu T tipo objektą kaip parametrą ir grąžintų tokį patį, jeigu sąraše jis egzistuoja.
+        public static T FindAndReturnIfEqual<T>(this List<T> list, T input)
+        {
+            int counter = 0;
+            foreach (T item in list)
+            {
+                if (item.Equals(input))
+                {
+                    counter++;
+                }
+            }
+            if (counter > 0)
+            {
+                return input;
+            }
+            else
+            {
+                return default; // <-- 40 min kol supratau, kaip gražinti Generic tuščią
+            }
+        }
+
+        // Parašykit extension metodą EveryOtherWord List<T> tipui, kuris grąžintų sąrašą sudaryta iš kas antro elemento.
+        public static List<T> RemoveEverySecondListItem<T>(this List<T> list)
+        {
+            int indexer = 2;
+            var list2 = new List<T>();
+            foreach (var item in list)
+            {
+                if (indexer % 2 == 0)
+                {
+                    list2.Add(item);
+                }
+                indexer++;
+            }
+            //System.Console.WriteLine("Sąrašas prieš metotą:");
+            //foreach (var item in list)
+            //{
+            //    System.Console.Write($"{item}, ");
+            //}
+            //System.Console.WriteLine();
+            //System.Console.WriteLine("Sąrašas po metodo:");
+            //foreach (var item in list2)
+            //{
+            //    System.Console.Write($"{item}, ");
+            //}
+            //System.Console.WriteLine();
+            return list2;
+        }
+
+        // Ar pavyktų sukurti extension metodų System.IO.File klasėi? Pavyzdžiui norint sukurt funkcionalumą grąžinantį kas antrą eilutę iš tekstinio failo.
+        //public List<int> ReadData()
+        //{
+        //    string path = $@"D:\GitHub\cs27_paskaita_ExceptionHandling\cs27_paskaita_ExceptionHandling\Rinkmena.csv";
+        //    var CSVLineReader = new StreamReader(path);
+
+        //    while (!CSVLineReader.EndOfStream)
+        //    {
+        //        var line = CSVLineReader.ReadLine();
+        //    }
+        //    return FileDataInAList;
+        //}
+
+        // (!) Path is static, and therefore you cannot create an extension method for it.Extension methods require an instance of an object.
+        // (!) You can't create extension method for static class.
+    }
+
+    //public static class StringExtensions
+    //{
+    //    public static int WordCount(this string str) // <-- čia this nutaiko į objektą iš kurio kviečiama
+    //{
+    //    return str.Split(new char[] { ' ', '.', '?', }, StringSplitOptions.RemoveEmptyEntries).Length;
+    //}
+    //}
+}
